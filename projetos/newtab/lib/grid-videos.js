670ddeba696e84d87f60786384f60384c121df8a
Cromak Technology <cromaksupport@gmail.com>
2026-09-11 00:18:28 -0300

    Modify grid-videos.js with new URLs and styles
    Updated background colors and URLs for video grid items.

--- projetos/newtab/lib/grid-videos.js
+++ projetos/newtab/lib/grid-videos.js
@@ -7,17 +7,17 @@ window.defaultTiles = window.defaultTiles || {};
 
 // Adiciona os dados exclusivos desta tela
 window.defaultTiles['grid-videos'] = [
-    { pos: 'pos-1', url: 'https://netflix.com.br/', bg: '#409da5', img: 'lib/thumbs/netflix.png', title: 'Netflix' },
-    { pos: 'pos-2', url: 'https://www.dailymotion.com/', bg: '#4849a6', img: 'lib/thumbs/dailymotion.png', title: 'dailymotion' },
+    { pos: 'pos-1', url: 'https://netflix.com.br/', bg: '#b1000e', img: 'lib/thumbs/netflix.png', title: 'Netflix' },
+    { pos: 'pos-2', url: 'https://dailymotion.com/', bg: '#4849a6', img: 'lib/thumbs/dailymotion.png', title: 'dailymotion' },
     { pos: 'pos-3', url: '#', bg: '#cd432d', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
-    { pos: 'pos-4', url: '#', bg: '#da8515', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
-    { pos: 'pos-5', url: '#', bg: '#7d9f2a', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
-    { pos: 'pos-6', url: 'https://www.primevideo.com', bg: '#672c9b', img: 'lib/thumbs/amazon-prime-video.png', title: 'Prime Video' },
-    { pos: 'pos-7', url: '#', bg: '#621354', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-4', url: '#', bg: '#438143', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-5', url: '#', bg: '#972453', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-6', url: 'https://primevideo.com', bg: '#303030', img: 'lib/thumbs/amazon-prime-video.png', title: 'Prime Video' },
+    { pos: 'pos-7', url: '#', bg: '#400b39', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
     { pos: 'pos-8', url: 'newtab.html', bg: '#f57c00', img: 'lib/thumbs/voltar.png', title: 'Voltar' },
-    { pos: 'pos-9', url: '#', bg: '#7d9f2a', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
-    { pos: 'pos-10', url: '#', bg: '#03417a', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
-    { pos: 'pos-11', url: '#', bg: '#409da5', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-9', url: '#', bg: '#486806', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-10', url: '#', bg: '#193f79', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
+    { pos: 'pos-11', url: '#', bg: '#5e151a', img: 'lib/thumbs/personalize.png', title: 'Personalizar' },
     { pos: 'pos-12', url: '#', bg: '#1a1a1a', img: 'lib/thumbs/personalize.png', title: 'Personalizar' }
 ];
 
